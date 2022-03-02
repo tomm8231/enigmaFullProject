dd706d2898190eabbf48a0aaba0dc01cb8327a4a
tomm8231 <tomm8231@stud.kea.dk>
2022-03-02 14:08:00 +0100

    Encryption and decryption are now working. Exit option added.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -12,34 +13,68 @@ public class Main {
 
 
     public void mainMenu(){
-        System.out.println("Which encryption/decryption method would you like to use?");
+        System.out.println("Which cipher method would you like to use?");
+        System.out.println("1) Ceasar");
+        System.out.println("2) Exit");
         int method = sc.nextInt();
-        switch (method) {
-            case 1 -> ceasarMethod();
-           // case 2 -> vitionaireMethod(); //LAVES MÅSKE
-           // case 3 -> exitProgramme(); //LAVES MÅSKE
-        }
         newLine();
+        switch (method) {
+            case 1 -> ceasarEncryptOrDecrypt();
+            case 2 -> System.exit(0);
+        }
+    }
+
+    public void finalOption() {
+        System.out.println("Would you like to:");
+        System.out.println("1) Do another encryption/decryption");
+        System.out.println("2) Exit");
+        int number = sc.nextInt();
+        switch(number) {
+            case 1 -> mainMenu();
+            case 2 -> System.exit(0);
+        }
     }
 
     public void newLine() {
         System.out.println();
     }
 
+    public void ceasarEncryptOrDecrypt() {
+        System.out.println("Please choose one of the cipher methods below followed by Enter:");
+        System.out.println("1) Encrypt");
+        System.out.println("2) Decrypt");
+        int number = sc.nextInt();
+        newLine();
+        switch(number) {
+            case 1 -> ceasarMethodEncrypt();
+            case 2 -> ceasarMethodDecrypt();
+        }
+        finalOption();
+    }
 
-    public void ceasarMethod() {
+    public void ceasarMethodDecrypt() {
         int shift = enterShift();
         newLine();
         String text = enterText();
+        StringBuilder decryptedText = caesarDecrypt(text, shift);
         newLine();
-        System.out.println("Encrypted text:");
+        System.out.println(decryptedText);
+        newLine();
+    }
+
+
+    public void ceasarMethodEncrypt() {
+        int shift = enterShift();
+        newLine();
+        String text = enterText();
         StringBuilder encryptedText = caesarEncrypt(text, shift);
         newLine();
         System.out.println(encryptedText);
+        newLine();
 
     }
 
-    private StringBuilder caesarEncrypt(String text, int shift) {
+    public StringBuilder caesarDecrypt(String text, int shift) {
         StringBuilder sb = new StringBuilder();
 
         char newLetter;
@@ -48,7 +83,29 @@ public class Main {
             char letter = text.charAt(i);
             int letterValue = letterToNumber(letter);
             if (letterValue != 0) {
-                int newLetterValue = shiftNumber(letterValue, shift);
+                int newLetterValue = shiftNumberDecrypt(letterValue, shift);
+                newLetter = numberToLetter(newLetterValue);
+            }
+            else {
+                newLetter = ' ';
+            }
+
+            sb.append(newLetter);
+
+        }
+        return sb;
+    }
+
+    public StringBuilder caesarEncrypt(String text, int shift) {
+        StringBuilder sb = new StringBuilder();
+
+        char newLetter;
+
+        for(int i = 0; i < text.length(); i++) {
+            char letter = text.charAt(i);
+            int letterValue = letterToNumber(letter);
+            if (letterValue != 0) {
+                int newLetterValue = shiftNumberEncrypt(letterValue, shift);
                 newLetter = numberToLetter(newLetterValue);
             }
             else {
@@ -65,7 +122,7 @@ public class Main {
         System.out.println("Enter your shift value:");
         int shiftValue = sc.nextInt();
         sc.nextLine(); // SCANNER BUG
-
+        newLine();
         System.out.println("Your shift value is " + shiftValue);
 
         return shiftValue;
@@ -74,9 +131,10 @@ public class Main {
     public String enterText() {
         System.out.println("Enter the text you'd like to encrypt");
 
-        String input = sc.nextLine();
+        String input = sc.nextLine().toUpperCase(Locale.ROOT);
+        newLine();
 
-        System.out.println("You are about to encrypt the following text: "  + input);
+        System.out.println("'" + input + "' encrypted is:");
 
         return input;
     }
@@ -91,7 +149,7 @@ public class Main {
         return numberTemp;
     }
 
-    public int shiftNumber(int numberTemp, int shift) {
+    public int shiftNumberEncrypt(int numberTemp, int shift) {
             int encryptNumberValue = numberTemp + shift;
 
             if (encryptNumberValue > 29) {
@@ -101,6 +159,19 @@ public class Main {
             return encryptNumberValue;
 
         }
+
+    public int shiftNumberDecrypt(int numberTemp, int shift) {
+        int decryptNumberValue = numberTemp - shift;
+
+        if (decryptNumberValue < 0) {
+            decryptNumberValue += 29;
+        } else if (decryptNumberValue == 0) {
+            decryptNumberValue = 29;
+        }
+
+        return decryptNumberValue;
+
+    }
 
 
     public char numberToLetter(int newNumber) {
